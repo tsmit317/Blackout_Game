ec8e5279aa949c69bd8fa994044ad5f942188cff
mid0ri <knguye22@gmail.com>
2016-04-27 07:48:40 -0400

    Add door animation detection
    when player gets on top of door animates opening and closing

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class DoorScript : MonoBehaviour {
-	//Since we have the GameManager there is no need to 
+
+	private BoxCollider2D doorBox;
+	private Animator doorAnim;
+	private bool canActivate;
+
+	void Start () {
+		doorBox = GetComponent<BoxCollider2D> ();
+		doorAnim = GetComponent<Animator> ();
+
+		canActivate = false;
+	}
 
 	public void EnterDoor(){
-
-
-
 		//For reusability - checks the current levels build index
 		if(SceneManager.GetActiveScene ().buildIndex == 2) 
 		{
@@ -18,5 +25,37 @@ public class DoorScript : MonoBehaviour {
 		{
 			SceneManager.LoadScene ("Level03");
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("ENTER DOOR BOX");
+		if (other.gameObject.CompareTag ("Player")) {
+			canActivate = true;     
+			StartCoroutine("animateOpening");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		Debug.Log ("EXIT DOOR BOX");
+		if (other.gameObject.CompareTag ("Player")) {
+			canActivate = false;
+			StartCoroutine("animateClosing");
+		}
+	}
+
+	IEnumerator animateOpening()
+	{
+		doorAnim.SetBool ("opening", true);
+		yield return new WaitForSeconds(0.5f);
+		doorAnim.SetBool ("isOpen", true);
+
+	}
+
+	IEnumerator animateClosing()
+	{
+		doorAnim.SetBool ("opening", false);
+		yield return new WaitForSeconds(0.5f);
+		doorAnim.SetBool ("isOpen", false);
+
 	}
 }
